6ec12b8fff57d8b44bcfe806cf3118259eb08850
sradevich <stanislav.radevich@zenitech.co.uk>
2022-05-23 14:06:03 +0300

    add password generator code

---
+++ main.py
@@ -0,0 +1,22 @@
+import random
+
+length = input('Please enter the length of the passowrd: ')
+
+if length.isdigit():
+    length = int(length)
+else:
+    print('Next time please use integer value as a length')
+
+lower = 'abcdefghijklmnopqrstuvwxyz'
+upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+numbers = '1234567890'
+special = '!@#$%^&*()_+-=[]{},.?'
+
+total = lower + upper + numbers + special
+
+password = ''
+
+for i in range(length):
+    password += random.choice(total)
+
+print(password)
